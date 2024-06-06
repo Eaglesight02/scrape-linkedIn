6e4e6f98ff95b63f585a61de803a07204cb00410
Manoj Kumar Pentapalli Pentapalli <108081278+Eaglesight02@users.noreply.github.com>
2024-06-06 08:52:43 +0000

    Getting experiences now

--- experiences.py
+++ experiences.py
@@ -1,15 +1,57 @@
+# experiences.py
 import os
+import json
 from dotenv import load_dotenv
 import google.generativeai as genai
 
-class generate_Experiences:
+class GenerateExperiences:
     def __init__(self) -> None:
-        experiences_dict = None
-        generation_config = None
-        safety_settings = None
-        model_name = None
-    
-    def __authenticate(self) -> bool:
+        # Generation Config
+        self._generation_config = {
+            "temperature": 1.0,
+            "top_p": 1,
+            "top_k": 0,
+            "max_output_tokens": 14000,
+            "response_mime_type": "text/plain" 
+        }
+
+        # Safety Settings for Gemini
+        self._safety_settings = [
+            {
+                "category": "HARM_CATEGORY_HARASSMENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+            },
+            {
+                "category": "HARM_CATEGORY_HATE_SPEECH",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+            },
+            {
+                "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+            },
+            {
+                "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                "threshold": "BLOCK_MEDIUM_AND_ABOVE",
+            },
+        ]
+
+        # System Instruction
+        self._system_instruction = '''
+            You are an agent that takes in a given piece of text and then creates a dictionary accordingly.
+            You are given a person's professional experience where you have to convert it into key, value pairs in the following manner where the keys signify:
+            Company, Role, Location, Start_Date, End_Date, Duration, Type_of_work(on-site, hybrid, remote), Description, Skills_Used.
+            Note that you have to only use Strings enclosed in triple quotes for the value of Description key, the rest using normal double quotes;
+            but include a list of skills for Skills_Used.
+            Enclose all of the generated dictionaries wherein each individual experience is taken as
+            key, value pairs: key: numbers(starting from zero), values being each professional experience; don't add anything else.
+            Generate only the required dictionary, but not as markdown text. 
+            You have to do the above tasks only for the text given, do not Hallucinate. Do not generate the dictionary as embedded code in Markdown.
+            Reply only the word 'YES' if you've understood the instruction.
+        '''
+
+
+    # Function to authenticate the API key given
+    def _authenticate(self) -> bool:
         try:
             if load_dotenv("dotfiles/.env"):
                 genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
@@ -19,20 +61,39 @@ class generate_Experiences:
             print("API KEY is invalid")
             return False
 
-    def initial_config(self) -> bool:
+
+    # Function to initialize the model with configurations
+    def _initial_config(self):
         try:
-            self.generatin_config = {
-                "temperature": 1.0,
-                "top_p": 1,
-                "top_k": 0,
-                "max_output_tokens": 14000,
-                "response_mime_type": "text/plain" 
-            }
-    
-    def get_Experiences(self, experiences_text = None) -> dict:
-        if self.__authenticate():
-            
-        else:
-            print("Cannot get the dictionary, taking all experiences in a single dictionary....")
-            self.experiences_dict["Experience"] = experiences_text
-        return self.experiences_dict.copy()
+            model = genai.GenerativeModel(
+                model_name="gemini-1.0-pro",
+                safety_settings=self._safety_settings,
+                generation_config=self._generation_config
+            )
+            return model
+        except Exception as e:
+            print(e)
+            return None
+
+
+    # Function to get the experiences from the given text
+    def get_experiences(self, experiences_text=None) -> str:
+        if self._authenticate():
+            model = self._initial_config()
+            if model is not None:
+                chat_session = model.start_chat()
+                initial_response = chat_session.send_message(self._system_instruction)
+                dictionary_response = None
+                if initial_response.text.lower() == "yes":
+                    dictionary_response = chat_session.send_message(experiences_text)
+                    if dictionary_response.text != "":
+                        # print(dictionary_response)
+                        return dictionary_response.text
+                        # return None
+                    else:
+                        print("Received empty text")
+                else:
+                    print("Received no response")
+            else:
+                print("Cannot get the dictionary, taking all experiences in a single dictionary....")
+        return ""
